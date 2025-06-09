528af6f30079f449223a122da3a310d70d152989
darkice9x <darkice9x@gmail.com>
2025-06-10 08:23:37 +0900

    add softwear kdiskmark

--- ubuntu_setting/01.init_install.sh
+++ ubuntu_setting/01.init_install.sh
@@ -51,7 +51,7 @@ sudo apt install -y libstdc++-14-dev
 sudo apt install -y vulkan-tools
 sudo ln -sf /usr/lib/aarch64-linux-gnu/dri /usr/lib/dri
 
-sudo apt install -y geany nemo mc cpufrequtils thunar net-tools dconf-editor smplayer inxi plocate
+sudo apt install -y geany nemo mc cpufrequtils thunar net-tools dconf-editor smplayer inxi plocate kdiskmark
 sudo apt install -y gdm3 gnome-tweaks gnome-shell-extension-manager chrome-gnome-shell
 
 #/etc/default/cpufrequtils 파일에 내용 작성
